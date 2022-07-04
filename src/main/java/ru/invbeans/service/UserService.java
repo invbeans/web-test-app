d30a03846e0391a7e6a5e5ef0c6e779fb17dbe5d
Vasileva Vlada <vlada.3234.vasileva@gmail.com>
2022-07-04 23:29:19 +0400

    user service uses email to find unique users

--- src/main/java/ru/invbeans/service/UserService.java
+++ src/main/java/ru/invbeans/service/UserService.java
@@ -2,12 +2,13 @@ package ru.invbeans.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import ru.invbeans.model.domain.User;
+import ru.invbeans.model.dto.UserDto;
 
 import java.util.List;
 
 public interface UserService {
     User findUserById(Long id);
     List<User> allUsers();
-    boolean saveUser(User user);
+    boolean saveUser(UserDto userDto);
     boolean deleteUser(Long id);
 }
--- src/main/java/ru/invbeans/service/impl/UserServiceImpl.java
+++ src/main/java/ru/invbeans/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import ru.invbeans.model.domain.Role;
 import ru.invbeans.model.domain.User;
+import ru.invbeans.model.dto.UserDto;
 import ru.invbeans.repository.RoleRepository;
 import ru.invbeans.repository.UserRepository;
 import ru.invbeans.service.UserService;
@@ -18,25 +19,27 @@ import java.util.List;
 import java.util.Optional;
 
 public class UserServiceImpl implements UserDetailsService, UserService {
-    @PersistenceContext
-    private EntityManager entityManager;
 
     @Autowired
     UserRepository userRepository;
 
     @Autowired
-    RoleRepository roleRepository;
-
-    @Autowired
     BCryptPasswordEncoder encoder;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-        Optional<User> userOptional = userRepository.findByUsername(username);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        Optional<User> userOptional = userRepository.findByEmail(email);
         if(!userOptional.isPresent()){
             throw new UsernameNotFoundException("User not found");
         }
-        return userOptional.get();
+        User user = userOptional.get();
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(String.valueOf(user.getRoles()))
+                .authorities("ROLE_USER")
+                .build();
     }
 
     @Override
@@ -51,13 +54,16 @@ public class UserServiceImpl implements UserDetailsService, UserService {
     }
 
     @Override
-    public boolean saveUser(User user) {
-        Optional<User> optionalUser = userRepository.findByUsername(user.getUsername());
-        if(!optionalUser.isPresent()){
+    public boolean saveUser(UserDto userDto) {
+        Optional<User> optionalUser = userRepository.findByUsername(userDto.getEmail());
+        if(optionalUser.isPresent()){
             return false;
         }
+        User user = new User();
+        user.setEmail(userDto.getEmail());
+        user.setUsername(userDto.getUsername());
         user.setRoles(Collections.singleton(new Role(1L, "ROLE_USER")));
-        user.setPassword(encoder.encode(user.getPassword()));
+        user.setPassword(encoder.encode(userDto.getPassword()));
         userRepository.save(user);
         return true;
     }
