7f7b966aa410950f6c4dbc099fa3b9b77a1f3e9c
Vasileva Vlada <vlada.3234.vasileva@gmail.com>
2022-07-04 23:30:31 +0400

    registration controller and mvc config

---
+++ src/main/java/ru/invbeans/controller/RegistrationController.java
@@ -0,0 +1,22 @@
+package ru.invbeans.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.invbeans.model.dto.UserDto;
+
+import javax.validation.Valid;
+
+@RequestMapping(RegistrationController.MAPPING)
+public interface RegistrationController {
+    String MAPPING = "/registration";
+
+    @GetMapping()
+    String registration(Model model);
+
+    @PostMapping
+    String addUser(@ModelAttribute("userForm") @Valid UserDto userForm, BindingResult bindingResult, Model model);
+}
